a1affebc1462c5766c62ab2c235ba6906edeb38b
Pranav Anilkumar <80296384+pranavanil47@users.noreply.github.com>
2024-09-02 15:18:49 +0400

    1

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args){
+        int[] x = new int[5];
+        long[][] l = new long[3][4];
+
+        for(int i= 0; i<5, i++){
+            x[1]= 10*i+1;
+
+        }
+
+    }
+}
---
+++ TypingPractice.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class Typingpractice{
+    public static void readOneLine() throws Exception
+    {
+        FileReader fr = new FileReader(fileName:"a.txt");
+        BufferedReader br = new BufferedReader()
+
+    }
+    public static void writeToFile()throws Exception 
+    {
+        FileWriter fr = new FileWriter(filename:"a.txt");
+        PrintWriter pw = new PrintWriter(fw);
+
+        pw.print(e:"First line written");
+        pw.println(b:"Hello");
+        pw.print(c:" second line written");
+
+        
+        
+    }
+}
